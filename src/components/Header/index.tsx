d2b8bb905b14e6068a074b24b8764051bdfa8a62
vhasousa <vitorhugo_as@hotmail.com>
2022-12-01 21:04:27 -0300

    Responsive header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,46 @@
+import { useState } from 'react'
+
+import { HeaderContainer } from './styles'
+
+import logoImg from '../../assets/logo.png'
+import menuIcon from '../../assets/menu.svg'
+import closeIcon from '../../assets/close.svg'
+
 export function Header() {
-  return <h1>Header</h1>
+  const [navActive, setNavActive] = useState(false)
+
+  return (
+    <HeaderContainer>
+      <nav className={`nav`}>
+        <a href="index.html">
+          <img src={logoImg} alt="" />
+        </a>
+
+        <ul className={`${navActive ? 'active' : ''} nav-list`}>
+          <li>
+            <a href="">Preços</a>
+          </li>
+          <li>
+            <a href="">Serviços</a>
+          </li>
+          <li>
+            <a href="">Contato</a>
+          </li>
+          <li>
+            <a href="">Localização</a>
+          </li>
+        </ul>
+
+        <div onClick={() => setNavActive(!navActive)} className={`mobile-menu`}>
+          <i>
+            {navActive ? (
+              <img src={closeIcon} alt="" />
+            ) : (
+              <img src={menuIcon} alt="" />
+            )}
+          </i>
+        </div>
+      </nav>
+    </HeaderContainer>
+  )
 }
